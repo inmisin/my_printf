47d5c0f647d47b78047b73fdd422a4023f086428
inmisin <omeroflu2@gmail.com>
2026-06-22 15:07:10 +0300

    printf

--- main.c
+++ main.c
@@ -48,34 +48,64 @@ void _putnbr(int n)
     
 }
 
+void _putstr(char *str)
+{
+    while(*str != '\0')
+    {
+        _putchar(*str);
+        str++;
+    }
+}
 
 
-void WriteValues(int n, ...)
+
+void my_printf(const char *format, ...)
 {
     va_list args;
-    va_start(args, n);
-    for (int i = 0; i < n; i++)
+    va_start(args, format);
+    while(*format != '\0')
     {
-        int a = va_arg(args, int);
-        _putnbr(a);
-        _putchar(*"\n");
+        if(*format == '%')
+        {
+            format++;
+            if(*format == 'd')
+            {
+                _putnbr(va_arg(args,int));
+            }
+            else if(*format == 'c')
+            {
+                _putchar(va_arg(args, int));
+            }
+            else if(*format == '%')
+            {
+                _putchar('%');
+            }
+            else if(*format == 's')
+            {
+                _putstr(va_arg(args, char *));
+            }
+        }
+        else
+        {
+            _putchar(*format);
+        }
+        format++;
     }
-    
     va_end(args);
 }
 
 int main()
 {
+    char a = 'a';
+    my_printf("%d %c %s \n", 1, a, "test");
+
     /*
     write(1, "Bir karakter giriniz\n", 21);
     char ch = _getchar();
     write(1, "\nGirdiginiz karakter: ", 22);
     write(1, &ch, 1);
     write(1, "\n", 1);
-    */
-    
-    WriteValues(2, -1,112);
-    
+    */ 
     
     return 0;
 }
